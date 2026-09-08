bf4bbde15798932777909164ac2cbd97197aa943
phgithum <2339553665@qq.com>
2026-09-08 21:26:53 +0800

    add notify script

---
+++ notify.py
@@ -0,0 +1,11 @@
+import json, sys, os, subprocess
+
+now = sys.argv[1] if len(sys.argv) > 1 else ''
+hook = os.environ.get('HOOK', '')
+cfg = json.load(open('config.json', encoding='utf-8'))
+for t in cfg.get('tasks', []):
+    if t.get('t') == now:
+        msg = t.get('msg', '')
+        body = json.dumps({'msgtype': 'text', 'text': {'content': msg}})
+        subprocess.run(['curl', '-s', '-X', 'POST', hook,
+                        '-H', 'Content-Type: application/json', '-d', body])
